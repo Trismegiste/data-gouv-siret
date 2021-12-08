61ed91af723ba87f31fd3cc4f51ee8b94df816c7
Trismegiste <flo@trismegiste.net>
2021-12-08 17:24:49 +0100

    adding option for restarting with an offset to continue the csv file

--- app.php
+++ app.php
@@ -10,5 +10,6 @@ $application = new Application("Videodrome", "1.6");
 
 // register commands
 $application->add(new App\Command\SiretRequest());
+$application->add(new \App\Command\SiretGoogle());
 
 $application->run();
--- src/Command/SiretRequest.php
+++ src/Command/SiretRequest.php
@@ -4,7 +4,9 @@ namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpClient\HttpClient;
 
 /**
  * Description of SiretRequest
@@ -21,40 +23,49 @@ class SiretRequest extends Command
     {
         $this->setDescription("Recherche de SIRET")
                 ->addArgument('source')
-                ->addArgument('sortie');
+                ->addArgument('sortie')
+                ->addOption('offset', null, InputOption::VALUE_REQUIRED, 'Décalage', 2);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $client = \Symfony\Component\HttpClient\HttpClient::create();
+        $client = HttpClient::create();
         $fch = fopen($input->getArgument('source'), 'r');
-        $dump = fopen($input->getArgument('sortie'), "w");
         $header = fgetcsv($fch);
 
+        $offset = $input->getOption('offset');
         $lineCount = 2;
-        fputcsv($dump, ['source', 'siret', 'titre', 'adresse', 'code postal']);
+
+        if (2 == $offset) {
+            $dump = fopen($input->getArgument('sortie'), "w");
+            fputcsv($dump, ['source', 'siret', 'titre', 'adresse', 'code postal']);
+        } else {
+            $dump = fopen($input->getArgument('sortie'), "w+");
+        }
+
         while ($row = fgetcsv($fch)) {
-            if (0 === strlen(trim($row[2]))) {
-                continue;
-            }
 
-            $recherche = "*** $lineCount : {$row[2]} {$row[3]} {$row[5]} ***";
-            $output->writeln($recherche);
+            if ((0 !== strlen(trim($row[2]))) && ($lineCount >= $offset)) {
 
-            $query = urlencode("{$row[2]} {$row[5]}");
-            $url = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=sirene_v3%40public&q=$query&rows=5&sort=datederniertraitementetablissement";
-            $response = $client->request('GET', $url);
-            $json = json_decode($response->getContent());
+                $recherche = "*** $lineCount : {$row[2]} {$row[3]} {$row[5]} ***";
+                $output->writeln($recherche);
 
-            if ($json->nhits == 1) {
-                $data = $json->records[0]->fields;
-                fputcsv($dump, [$recherche, "{$data->siret} ", $data->l1_adressage_unitelegale, $data->adresseetablissement, $data->codepostaletablissement]);
-            } else {
-                fputcsv($dump, []);
+                $query = urlencode("{$row[2]} {$row[5]}");
+                $url = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=sirene_v3%40public&q=$query&rows=5&sort=datederniertraitementetablissement";
+                $response = $client->request('GET', $url);
+                $json = json_decode($response->getContent());
+
+                if ($json->nhits == 1) {
+                    $data = $json->records[0]->fields;
+                    fputcsv($dump, [$recherche, "{$data->siret} ", $data->l1_adressage_unitelegale, $data->adresseetablissement, $data->codepostaletablissement]);
+                } else {
+                    fputcsv($dump, []);
+                }
+
+                sleep(1);
             }
 
             $lineCount++;
-            sleep(1);
         }
 
         fclose($dump);
